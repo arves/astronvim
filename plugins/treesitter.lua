7b0f50dd41459691ccc46506a1864f60937565c8
Arve Sellesbakk <arve.sellesbakk@gmail.com>
2023-11-05 14:32:03 +0100

    restructure treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -14,7 +14,11 @@ return {
     opts = function(_, opts)
       if opts.ensure_installed ~= "all" then
         opts.ensure_installed = utils.list_insert_unique(
-          opts.ensure_installed, { "php", "python", "toml" }
+          opts.ensure_installed, { 
+            "php", 
+            "python", 
+            "toml",
+          }
         )
       end
     end,
