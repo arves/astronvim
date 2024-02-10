859ba3b370f566cb251b0170383bdc40aeb760de
Arve Sellesbakk <arve.sellesbakk@gmail.com>
2024-02-11 00:15:44 +0100

    Make telescope borderless

--- plugins/tokyonight.lua
+++ plugins/tokyonight.lua
@@ -1,6 +1,41 @@
 return {
   "folke/tokyonight.nvim",
-  opts = {
-    transparent = false,
-    }
+  config = function()
+    -- Now we make telescope borderless
+    require("tokyonight").setup({
+      on_highlights = function(hl, c)
+        local prompt = "#2d3149"
+        hl.TelescopeNormal = {
+          bg = c.bg_dark,
+          fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePromptNormal = {
+          bg = prompt,
+        }
+        hl.TelescopePromptBorder = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePromptTitle = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePreviewTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+      end,
+    })
+  end,
+  -- opts = {
+  --   transparent = false,
+  --   }
 }
