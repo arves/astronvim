d4de8626303f26fcd993de6643b55ff2592d401a
Arve Sellesbakk <arve.sellesbakk@gmail.com>
2024-02-14 17:08:47 +0100

    trimming down telescope

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -1,48 +1,17 @@
 return {
   "nvim-telescope/telescope.nvim",
-  lazy = true,
   dependencies = {
     {
-      "nvim-telescope/telescope-fzf-native.nvim", enabled = vim.fn.executable "make" == 1, build = "make",
-      "nvim-lua/plenary.nvim",
       "folke/trouble.nvim",
     },
   },
-  cmd = "Telescope",
-  opts = function()
-    require("telescope").load_extension("noice")
-    local actions = require "telescope.actions"
-    local get_icon = require("astronvim.utils").get_icon
-    local trouble = require("trouble.providers.telescope")
-    return {
-      defaults = {
-        git_worktrees = vim.g.git_worktrees,
-        prompt_prefix = get_icon("Selected", 1),
-        selection_caret = get_icon("Selected", 1),
-        path_display = { "truncate" },
-        sorting_strategy = "ascending",
-        layout_config = {
-          horizontal = { prompt_position = "top", preview_width = 0.55 },
-          vertical = { mirror = false },
-          width = 0.87,
-          height = 0.80,
-          preview_cutoff = 120,
-        },
-        mappings = {
-            i = { 
-              ["<c-o>"] = trouble.open_with_trouble,
-              ["<C-n>"] = actions.cycle_history_next,
-              ["<C-p>"] = actions.cycle_history_prev,
-              ["<C-j>"] = actions.move_selection_next,
-              ["<C-k>"] = actions.move_selection_previous,
-            },
-            n = {
-              ["<c-o>"] = trouble.open_with_trouble,
-              q = actions.close,
-            },
-          },
-      },
-    }
+  config = function(plugins, opts)
+    local trouble = require "trouble.providers.telescope"
+    -- Now we add our custom bindings for trouble
+    opts.defaults.mappings.i["<c-o>"] = trouble.open_with_trouble
+    opts.defaults.mappings.n["<c-o>"] = trouble.open_with_trouble
+    require "plugins.configs.telescope"(plugins, opts)
+    local telescope = require "telescope"
+    telescope.load_extension "noice"
   end,
-  config = require "plugins.configs.telescope",
 }
