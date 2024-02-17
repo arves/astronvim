158ce602fdcad7afb2a6a7cfe90236f3c8d08fce
Arve Sellesbakk <arve.sellesbakk@gmail.com>
2024-02-17 03:34:00 +0100

    Adjusting transparency and borderless behaviour for telescope

--- plugins/tokyonight.lua
+++ plugins/tokyonight.lua
@@ -3,62 +3,34 @@ return {
   config = function()
     -- Now we make telescope borderless
     require("tokyonight").setup {
-      transparent = true,
       on_highlights = function(hl, c)
         local prompt = "#2d3149"
-        --   hl.TelescopeNormal = {
-        --     bg = "none",
-        --     fg = "none",
-        --   }
         hl.TelescopeNormal = {
           bg = c.bg_dark,
           fg = c.fg_dark,
         }
-        --   hl.TelescopeBorder = {
-        --     bg = "none",
-        --     fg = "none",
-        --   }
         hl.TelescopeBorder = {
-          bg = prompt,
-          fg = prompt,
+          bg = c.bg_dark,
+          fg = c.bg_dark,
         }
-        --   hl.TelescopePromptNormal = {
-        --     bg = "none",
-        --   }
         hl.TelescopePromptNormal = {
           bg = prompt,
         }
-        --   hl.TelescopePromptBorder = {
-        --     bg = "none",
-        --     fg = "none",
-        --   }
         hl.TelescopePromptBorder = {
           bg = prompt,
           fg = prompt,
         }
-        --   hl.TelescopePromptTitle = {
-        --     bg = "none",
-        --     fg = "none",
-        --   }
         hl.TelescopePromptTitle = {
           bg = prompt,
-          fg = "#a4aed6",
+          fg = prompt,
         }
-        --   hl.TelescopePreviewTitle = {
-        --     bg = "none",
-        --     fg = "none",
-        --   }
         hl.TelescopePreviewTitle = {
-          bg = prompt,
-          fg = "#a4aed6",
+          bg = c.bg_dark,
+          fg = c.bg_dark,
         }
-        --   hl.TelescopeResultsTitle = {
-        --     bg = "none",
-        --     fg = "none",
-        --   }
         hl.TelescopeResultsTitle = {
-          bg = prompt,
-          fg = "#a4aed6",
+          bg = c.bg_dark,
+          fg = c.bg_dark,
         }
       end,
     }
